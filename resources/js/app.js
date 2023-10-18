dad215c20fad645d9555664a0548fee7c4b69c5f
gedeastu <gede.astu@gmail.com>
2023-10-18 20:04:22 +0700

    up

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+import 'flowbite';
+import './bootstrap';
+import 'laravel-datatables-vite';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
---
+++ resources/views/user/daftarPengguna.blade.php
@@ -0,0 +1,30 @@
+
+{{-- 6706220050-AHMAD FAZA AL FARISI-D3IF 46-04 --}}
+
+<x-app-layout>
+    <x-slot name="header">
+        <div class="flex justify-between items-center">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Daftar Pengguna') }}
+            </h2>
+            <a href="{{ route('user.registrasi') }}">
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">REGISTRASI PENGGUNA</button>
+            </a>
+        </div>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="w-full">
+                        {{ $dataTable->table() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @push('scripts')
+        {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
+    @endpush
+</x-app-layout>
